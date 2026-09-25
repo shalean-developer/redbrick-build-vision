4d26d49d543e3b62c39d9379dc66f7afd57200e3
Farai Chitekedza <developer@shalean.com>
2026-09-25 13:49:06 +0200

    SEO: add canonical WebSite entity to root schema

--- components/json-ld.tsx
+++ components/json-ld.tsx
@@ -6,41 +6,56 @@ import {
   siteOrigin,
 } from "@/lib/site";
 
-const organizationJsonLd = {
+const organizationId = `${siteOrigin}/#organization`;
+const websiteId = `${siteOrigin}/#website`;
+
+const structuredData = {
   "@context": "https://schema.org",
-  "@type": "GeneralContractor",
-  "@id": `${siteOrigin}/#organization`,
-  name: siteName,
-  alternateName: "Team Edlick PVT Ltd",
-  url: siteOrigin,
-  description: defaultDescription,
-  logo: `${siteOrigin}/brand/teamedlick-logo.png`,
-  telephone: primaryBusinessPhone,
-  address: {
-    "@type": "PostalAddress",
-    streetAddress: businessAddress.streetAddress,
-    addressLocality: businessAddress.addressLocality,
-    addressRegion: businessAddress.addressRegion,
-    postalCode: businessAddress.postalCode,
-    addressCountry: businessAddress.addressCountry,
-  },
-  areaServed: {
-    "@type": "City",
-    name: "Cape Town",
-    containedInPlace: {
-      "@type": "AdministrativeArea",
-      name: "Western Cape",
-      containedInPlace: { "@type": "Country", name: "South Africa" },
-    },
-  },
-  contactPoint: [
+  "@graph": [
     {
-      "@type": "ContactPoint",
-      name: "Sam",
-      telephone: "+27-68-594-3091",
-      contactType: "customer service",
-      areaServed: "Cape Town, Western Cape, ZA",
-      availableLanguage: ["English"],
+      "@type": "GeneralContractor",
+      "@id": organizationId,
+      name: siteName,
+      alternateName: "Team Edlick PVT Ltd",
+      url: siteOrigin,
+      description: defaultDescription,
+      logo: `${siteOrigin}/brand/teamedlick-logo.png`,
+      telephone: primaryBusinessPhone,
+      address: {
+        "@type": "PostalAddress",
+        streetAddress: businessAddress.streetAddress,
+        addressLocality: businessAddress.addressLocality,
+        addressRegion: businessAddress.addressRegion,
+        postalCode: businessAddress.postalCode,
+        addressCountry: businessAddress.addressCountry,
+      },
+      areaServed: {
+        "@type": "City",
+        name: "Cape Town",
+        containedInPlace: {
+          "@type": "AdministrativeArea",
+          name: "Western Cape",
+          containedInPlace: { "@type": "Country", name: "South Africa" },
+        },
+      },
+      contactPoint: [
+        {
+          "@type": "ContactPoint",
+          name: "Sam",
+          telephone: "+27-68-594-3091",
+          contactType: "customer service",
+          areaServed: "Cape Town, Western Cape, ZA",
+          availableLanguage: ["English"],
+        },
+      ],
+    },
+    {
+      "@type": "WebSite",
+      "@id": websiteId,
+      url: siteOrigin,
+      name: siteName,
+      inLanguage: "en-ZA",
+      publisher: { "@id": organizationId },
     },
   ],
 };
@@ -49,7 +64,7 @@ export function JsonLd() {
   return (
     <script
       type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationJsonLd) }}
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
     />
   );
 }
